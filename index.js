ad81bc8c5614adfe69c4de03d4b8167a0e638f48
unknown <ahmad.forhat@gmail.com>
2019-12-30 10:16:23 -0300

    arrumando tudo

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const main = async function() {
         method: "GET",
         url:urlJuno,
         headers:{
-            "token": "2sKUvYTceiMgDoSo3xTsXefMYUDACTU98Ryqh5ypByBU",
+            "token": process.env.tokensheets,
             "Authorization": authJuno
         },
         json: true
@@ -55,7 +55,7 @@ const main = async function() {
             "spreadsheetId": process.env.sheetsId,
             "range": "BoletosPag!A2",
             "resource": {
-                "values": [["1231231","231231231"]]
+                "values": arrayID
             },
             "valueInputOption": "raw"
         },
